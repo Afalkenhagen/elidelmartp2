3a91997e40d46b16d6a4285eda0a68592c4d6340
Afalkenhagen <ahau_buluk@protonmail.ch>
2021-12-15 21:54:58 -0300

    feat: navbar and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
