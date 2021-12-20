65e84dfae58c5ad8cbd19d3d6fbd81431fd9d862
Afalkenhagen <ahau_buluk@protonmail.ch>
2021-12-20 20:58:46 -0300

    feat: footer y article(cards)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { ArticleComponent } from './components/article/article.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     LogoComponent,
-    CarouselComponent
+    CarouselComponent,
+    ArticleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
